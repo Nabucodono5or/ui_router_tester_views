c96666540cb21f9923a63747596d9519fc7f33ac
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-01-07 00:24:52 -0200

    criado primeiro componente

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,6 @@
 import angular from "angular";
 import homeComponent from "./home.component";
 
-let homeModule = angular.module("homeModule", []).component(homeComponent).name;
+let homeModule = angular.module("homeModule", []).component('home', homeComponent).name;
 
 export default homeModule
